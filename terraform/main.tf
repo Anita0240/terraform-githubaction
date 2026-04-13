3f242f0300ab993156eb9e1f19075dccaeeace12
unknown <anitapandey9575@gmail.com>
2026-04-13 15:47:09 +0530

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,11 @@
 provider "aws" {
-  region     = "ap-south-1" 
-  }
+  region     = "ap-south-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
 
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 
 resource "aws_security_group" "doctor_ai_sg" {
   name        = "doctor_ai_sg"
